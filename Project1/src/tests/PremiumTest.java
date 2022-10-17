ad989d5d7e967c08a7233f3e82b177bea3172b7e
Dylan Pina <dillon_idgafos@yahoo.com>
2022-10-17 00:01:14 -0400

    Added test for Premium member membership fee

---
+++ Project1/src/tests/PremiumTest.java
@@ -0,0 +1,17 @@
+package tests;
+
+import gymmanager.Premium;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PremiumTest {
+
+    @Test
+    void membershipFee() {
+        Premium premiumMember = new Premium();
+        double expectedResult = 659.89;
+        double result = premiumMember.membershipFee();
+        assertEquals(expectedResult, result);
+    }
+}
